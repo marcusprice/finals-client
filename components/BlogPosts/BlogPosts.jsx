76058c79e88b54e411d52372df93eca4b1a00bf6
Marcus Price <marcusprice88@gmail.com>
2020-09-08 10:21:34 -0700

    added posts component

--- components/BlogPosts/BlogPosts.jsx
+++ components/BlogPosts/BlogPosts.jsx
@@ -4,10 +4,16 @@ import Post from '../Post';
 const BlogPosts = props => {
     console.log(props.posts);
 
+    const handleInitialPosts = () => {
+        return props.posts.map(post => (
+            <Post title={post.title} author={post.created_by.firstname} postContent={post.content}/>
+        ))
+    }
+
     return(
         <section className={styles.container}>
             <h2>Posts</h2>
-
+            {handleInitialPosts()}
         </section>
     );
 };
